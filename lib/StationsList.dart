aa751f176637e88d3ed4cf47f734d6a3396ea1fd
Jonas Schmid <jonas.schmid@gmail.com>
2020-06-17 19:12:11 +0200

    Updated view of the stations

--- lib/StationsList.dart
+++ lib/StationsList.dart
@@ -7,45 +7,48 @@ class StationsList extends StatelessWidget {
       {Key key,
       @required SolverResponse solverResult,
       @required this.removeStationCallback})
-      : _stations = _buildStations(solverResult),
+      : _legs = _buildLegs(solverResult),
         super(key: key);
 
-  final List<Station> _stations;
+  final List<ListLeg> _legs;
   final ValueChanged<String> removeStationCallback;
 
-  static List<Station> _buildStations(SolverResponse solverResult) {
-    final List<Station> stationsBuilder = [];
+  static List<ListLeg> _buildLegs(SolverResponse solverResult) {
+    final List<ListLeg> legsBuilder = [];
 
     if (solverResult != null) {
-      var firstStationId = solverResult.legs.first.from;
-      var firstStation = solverResult.stations
-          .firstWhere((station) => station.id == firstStationId);
-      stationsBuilder.add(firstStation);
+      final Map<String, Station> _stationById = {};
+      for (final station in solverResult.stations) {
+        _stationById[station.id] = station;
+      }
 
       for (final leg in solverResult.legs) {
-        var stationId = leg.to;
-        var station = solverResult.stations
-            .firstWhere((station) => station.id == stationId);
-        stationsBuilder.add(station);
+        var fromStation = _stationById[leg.from];
+        var toStation = _stationById[leg.to];
+        legsBuilder.add(ListLeg(
+            fromId: leg.from,
+            fromStation: fromStation.name,
+            toStation: toStation.name,
+            durationMinutes: leg.durationMinutes));
       }
     }
-    return stationsBuilder.toList(growable: false);
+    return legsBuilder.toList(growable: false);
   }
 
   Widget _buildList() {
     return ListView.builder(
-        itemCount: _stations.length,
+        itemCount: _legs.length,
         itemBuilder: (context, i) {
-          return _buildRow(_stations[i]);
+          return _buildRow(_legs[i]);
         });
   }
 
-  Widget _buildRow(Station station) {
+  Widget _buildRow(ListLeg leg) {
     return Dismissible(
-      key: Key(station.id),
+      key: Key(leg.fromId),
       direction: DismissDirection.endToStart,
       onDismissed: (direction) {
-        removeStationCallback(station.id);
+        removeStationCallback(leg.fromId);
       },
       background: Container(
         alignment: Alignment.centerRight,
@@ -57,7 +60,9 @@ class StationsList extends StatelessWidget {
         ),
       ),
       child: ListTile(
-        title: Text(station.name),
+        title: Text(leg.fromStation),
+        subtitle: Text(leg.toStation),
+        trailing: Text(leg.textDuration()),
       ),
     );
   }
@@ -65,5 +70,33 @@ class StationsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return _buildList();
+  }
+}
+
+class ListLeg {
+  ListLeg({
+    @required this.fromId,
+    @required this.fromStation,
+    @required this.toStation,
+    @required this.durationMinutes,
+  });
+
+  final String fromId;
+  final String fromStation;
+  final String toStation;
+  final int durationMinutes;
+
+  String textDuration() {
+    final duration = Duration(minutes: durationMinutes);
+    final formatted = duration.toString().split('.').first;
+    final split = formatted.split(':');
+    final hours = split[0];
+    final minutes = split[1];
+
+    if (hours == "0") {
+      return "$minutes min";
+    } else {
+      return "$hours h $minutes min";
+    }
   }
 }
